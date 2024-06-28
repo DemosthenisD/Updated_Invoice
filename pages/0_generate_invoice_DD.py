930e51579d0ab3a8f2f2a46f7c566ce873eab7ce
DemosthenisD <67370261+DemosthenisD@users.noreply.github.com>
2024-06-28 18:03:27 +0300

    Update 0_generate_invoice_DD.py

--- pages/0_generate_invoice_DD.py
+++ pages/0_generate_invoice_DD.py
@@ -276,7 +276,7 @@ def main():
                 format_option = st.radio("Select download format", ["DOCX", "PDF"],  key="format_option")
 
 
-        VAT_Amount = (amount*vat)/100
+        VAT_Amount = (amount * vat)/100
         Expenses_Net_Amount = 0
         Expenses_VAT_Amount = 0 #(Expenses_Net_Amount*vat)/100
         
