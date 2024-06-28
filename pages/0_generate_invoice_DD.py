be8f7397fec24e8c6f61d522a942a032e3bc2247
DemosthenisD <67370261+DemosthenisD@users.noreply.github.com>
2024-06-28 18:42:59 +0300

    Update 0_generate_invoice_DD.py

--- pages/0_generate_invoice_DD.py
+++ pages/0_generate_invoice_DD.py
@@ -226,6 +226,7 @@ def main():
             year = date.year
         with col3:            # DD_28062024: previously--> with col2
             amount = st.number_input("Amount")
+            amount = "{:,}".format(amount)
         with col1:            # DD_28062024: New addition
             Pre_invoice_no = len(df_Invoice_List[df_Invoice_List['Year'] == year]) +1
             #invoice_no= year &"/"& st.text_input(label="invoice No", value=Pre_invoice_no) # DD_28062024: previously--> _invoice_no = len(st.session_state.invoices) + 1
